415f56d6a6b93a2a3ab5427445d488ea50ca4dd4
guilhermesegura <guilhermesegura26@gmail.com>
2025-11-20 18:54:41 -0300

    config.p

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,13 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('./.env')
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_NAME = os.getenv('DB_NAME', 'barbearia')
     DB_USER = os.getenv('DB_USER', 'postgres')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'Hrvl2604!')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_PORT = os.getenv('DB_PORT', '5432')
     JWT_EXPIRATION_HOURS = 24
